6b77fc164b87fb7c9b9f1ed37ebdf47ae3944706
ahmed <foudiahmed82@gmail.com>
2025-02-24 15:15:50 +0000

    patient repository

---
+++ src/main/java/ahmed/foudi/DocPat/dao/PatientRepository.java
@@ -0,0 +1,13 @@
+package ahmed.foudi.DocPat.dao;
+
+import ahmed.foudi.DocPat.entities.Patient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+}
