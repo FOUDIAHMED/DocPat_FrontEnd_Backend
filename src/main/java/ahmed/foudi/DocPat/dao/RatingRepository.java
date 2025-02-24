d192f7fd695a7ea8a5a12545ea9e9dc61c5ab791
ahmed <foudiahmed82@gmail.com>
2025-02-24 16:06:00 +0000

    Rating repository

---
+++ src/main/java/ahmed/foudi/DocPat/dao/RatingRepository.java
@@ -0,0 +1,9 @@
+package ahmed.foudi.DocPat.dao;
+
+import ahmed.foudi.DocPat.entities.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, Long> {
+}
