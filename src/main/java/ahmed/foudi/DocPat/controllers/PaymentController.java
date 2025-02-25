6e512b955c6dea2db559d7444df23e22a5415ee1
ahmed <foudiahmed82@gmail.com>
2025-02-25 10:24:02 +0000

    Payment  controller

---
+++ src/main/java/ahmed/foudi/DocPat/controllers/PaymentController.java
@@ -0,0 +1,78 @@
+package ahmed.foudi.DocPat.controllers;
+
+
+import ahmed.foudi.DocPat.entities.Payment;
+import ahmed.foudi.DocPat.services.interfaces.PaymentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/payments")
+@RequiredArgsConstructor
+
+public class PaymentController {
+
+    private final PaymentService paymentService;
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping
+    public ResponseEntity<List<Payment>> getAllPayments() {
+        return ResponseEntity.ok(paymentService.findAll());
+    }
+
+    @PreAuthorize("hasRole('DOCTOR')")
+    @GetMapping("/doctor/{doctorId}")
+    public ResponseEntity<List<Payment>> getDoctorPayments(@PathVariable Long doctorId) {
+        return ResponseEntity.ok(paymentService.findByDoctorId(doctorId));
+    }
+
+    @PreAuthorize("hasRole('PATIENT')")
+    @GetMapping("/patient/{patientId}")
+    public ResponseEntity<List<Payment>> getPatientPayments(@PathVariable Long patientId) {
+        return ResponseEntity.ok(paymentService.findByPatientId(patientId));
+    }
+
+    @PreAuthorize("hasAnyRole('DOCTOR', 'PATIENT', 'ADMIN')")
+    @GetMapping("/{id}")
+    public ResponseEntity<Payment> getPaymentById(@PathVariable Long id) {
+        return ResponseEntity.ok(paymentService.findById(id));
+    }
+
+    @PreAuthorize("hasRole('PATIENT')")
+    @PostMapping
+    public ResponseEntity<Payment> createPayment(@RequestBody Payment payment) {
+        return ResponseEntity.ok(paymentService.save(payment));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @PutMapping("/{id}")
+    public ResponseEntity<Payment> updatePayment(@PathVariable Long id, @RequestBody Payment payment) {
+        payment.setId(id);
+        return ResponseEntity.ok(paymentService.save(payment));
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletePayment(@PathVariable Long id) {
+        paymentService.deleteById(id);
+        return ResponseEntity.noContent().build();
+    }
+
+//    @PreAuthorize("hasRole('PATIENT')")
+//    @PostMapping("/{id}/process")
+//    public ResponseEntity<Payement> processPayment(@PathVariable Long id) {
+//        return ResponseEntity.ok(paymentService.processPayment(id));
+//    }
+
+//    @PreAuthorize("hasRole('ADMIN')")
+//    @PostMapping("/{id}/refund")
+//    public ResponseEntity<Payement> refundPayment(@PathVariable Long id) {
+//        return ResponseEntity.ok(paymentService.refundPayment(id));
+//    }
+
+
+}
